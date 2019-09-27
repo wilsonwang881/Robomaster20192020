520dab57ac0b9af59a3467899442ec0ac3968fbb
Sid Prabhakaran <s2siddhu@gmail.com>
2019-09-27 18:12:28 -0500

    INS Task Translation
    Translated by Sid Prabhakaran

--- user/APP/INS_task/INS_task.h
+++ user/APP/INS_task/INS_task.h
@@ -1,14 +1,20 @@
 /**
   ****************************(C) COPYRIGHT 2016 DJI****************************
   * @file       INSTask.c/h
-  * @brief      ��Ҫ����������mpu6500��������ist8310�������̬���㣬�ó�ŷ���ǣ�
-  *             �ṩͨ��mpu6500��data ready �ж�����ⲿ�������������ݵȴ��ӳ�
-  *             ͨ��DMA��SPI�����ԼCPUʱ�䣬�ṩע�Ͷ�Ӧ�ĺ궨�壬�ر�DMA��
-  *             DR���ⲿ�жϵķ�ʽ.
-  * @note       SPI �������ǳ�ʼ����ʱ����Ҫ����2MHz��֮���ȡ���������20MHz
+  * @brief      主要利用陀螺仪mpu6500，磁力计ist8310，完成姿态解算，得出欧拉角，
+  *             提供通过mpu6500的data ready 中断完成外部触发，减少数据等待延迟
+  *             通过DMA的SPI传输节约CPU时间，提供注释对应的宏定义，关闭DMA，
+  *             DR的外部中断的方式.
+  * 
+  *             Seems to use the MPU6500(gyro+accelerometer) and IST8310(magnetometer)
+  *             to calculate 3d position of turret, and the euler angle. Through the SPI
+  *             of DMA transmission save CPU time, provide the annotation corresponding 
+  *             macro definition, close the DMA
+  * 
+  * @note       SPI 在陀螺仪初始化的时候需要低于2MHz，之后读取数据需低于20MHz
   * @history
   *  Version    Date            Author          Modification
-  *  V1.0.0     Dec-26-2018     RM              1. ���
+  *  V1.0.0     Dec-26-2018     RM              1. 完成
   *
   @verbatim
   ==============================================================================
@@ -21,47 +27,47 @@
 #define INS_Task_H
 #include "main.h"
 
-#define USE_IST8310 //�Ƿ�ʹ��IST8310�����ƣ���ʹ��ע�Ͷ���
+#define USE_IST8310 //是否使用IST8310磁力计，不使用注释定义
 
-#define MPU6500_USE_DATA_READY_EXIT //�Ƿ�ʹ��MPU6500���ⲿ�жϣ���ʹ��ע�Ͷ���
+#define MPU6500_USE_DATA_READY_EXIT //是否使用MPU6500的外部中断，不使用注释定义
 
-#define MPU6500_USE_SPI_DMA //�Ƿ�ʹ��SPI��DMA���䣬��ʹ��ע�Ͷ���
+#define MPU6500_USE_SPI_DMA //是否使用SPI的DMA传输，不使用注释定义
 
-//�������IST8310��DMA����23���ֽڣ�������ã���7���ֽڣ�Ϊ16���ֽ�
+//如果用了IST8310，DMA传输23个字节，如果不用，少7个字节，为16个字节
 #if defined(USE_IST8310)
 #define DMA_RX_NUM 23
 #else
 #define DMA_RX_NUM 16
 #endif
 
-//mpu6500ԭʼ�����ڻ�����buf��λ��
+//mpu6500原始数据在缓冲区buf的位置
 #ifdef MPU6500_USE_SPI_DMA
 #define MPU6500_RX_BUF_DATA_OFFSET 1
 #else
 #define MPU6500_RX_BUF_DATA_OFFSET 0
 #endif
 
-//ist83100ԭʼ�����ڻ�����buf��λ��
+//ist83100原始数据在缓冲区buf的位置
 #ifdef MPU6500_USE_SPI_DMA
 #define IST8310_RX_BUF_DATA_OFFSET 16
 #else
 #define IST8310_RX_BUF_DATA_OFFSET 15
 #endif
 
-#define MPU6500_TEMPERATURE_PID_KP 1600.0f //�¶ȿ���PID��kp
-#define MPU6500_TEMPERATURE_PID_KI 0.2f    //�¶ȿ���PID��ki
-#define MPU6500_TEMPERATURE_PID_KD 0.0f    //�¶ȿ���PID��kd
+#define MPU6500_TEMPERATURE_PID_KP 1600.0f //温度控制PID的kp
+#define MPU6500_TEMPERATURE_PID_KI 0.2f    //温度控制PID的ki
+#define MPU6500_TEMPERATURE_PID_KD 0.0f    //温度控制PID的kd
 
-#define MPU6500_TEMPERATURE_PID_MAX_OUT 4500.0f  //�¶ȿ���PID��max_out
-#define MPU6500_TEMPERATURE_PID_MAX_IOUT 4400.0f //�¶ȿ���PID��max_iout
+#define MPU6500_TEMPERATURE_PID_MAX_OUT 4500.0f  //温度控制PID的max_out
+#define MPU6500_TEMPERATURE_PID_MAX_IOUT 4400.0f //温度控制PID的max_iout
 
-#define INS_DELTA_TICK 1 //������õļ��
+#define INS_DELTA_TICK 1 //任务调用的间隔
 
-#define INS_TASK_INIT_TIME 7 //����ʼ���� delay һ��ʱ��
+#define INS_TASK_INIT_TIME 7 //任务开始初期 delay 一段时间
 
-#define MPU6500_TEMP_PWM_MAX 5000 //mpu6500�����¶ȵ�����TIM������ֵ������PWM���Ϊ MPU6500_TEMP_PWM_MAX - 1
+#define MPU6500_TEMP_PWM_MAX 5000 //mpu6500控制温度的设置TIM的重载值，即给PWM最大为 MPU6500_TEMP_PWM_MAX - 1
 
-//��ȡ��̬��ָ���ַ�󣬶�Ӧ��̬�ǵĵ�ַƫ���� fp32����
+//获取姿态角指针地址后，对应姿态角的地址偏移量 fp32类型
 #define INS_YAW_ADDRESS_OFFSET 0
 #define INS_PITCH_ADDRESS_OFFSET 1
 #define INS_ROLL_ADDRESS_OFFSET 2
