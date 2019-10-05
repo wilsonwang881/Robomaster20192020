0a0be649851b00ff06dc500ddfb1500282b867f8
wilsonwang881 <mitfans881@gmail.com>
2019-10-04 19:42:51 -0500

    finished translating the FreeRTOS_middleware folder

--- user/APP/FreeRTOS_middleware/FreeRTOS_middleware.c
+++ user/APP/FreeRTOS_middleware/FreeRTOS_middleware.c
@@ -2,9 +2,11 @@
   ****************************(C) COPYRIGHT 2016 DJI****************************
   * @file       freeRTOS_middle.c/h
   * @brief      freeRTOS的中间层，将滴答计时中断和统计任务用时的接口函数放到这里.
+  *             middleware for freeRTOS
+  *             place the interface function for tick timer interrupt and task stats here
   * @history
   *  Version    Date            Author          Modification
-  *  V1.0.0     Dec-26-2018     RM              1. 完成
+  *  V1.0.0     Dec-26-2018     RM              1. 完成 complete
   *
   @verbatim
   ==============================================================================
@@ -22,6 +24,7 @@
 #include "task.h"
 
 //设置调度中断定时器配置
+//set schedule interrupt timer parameters
 void vPortSetupTimerInterrupt(void)
 {
 }
@@ -36,6 +39,7 @@ void SysTick_Handler(void)
 }
 
 //利用tim3作为任务统计用时，所有任务目前测试cpu利用率
+//test the current CPU utilization rate when using tim3 as the task stat data collector
 volatile uint64_t FreeRTOSRunTimeTicks = 0;
 
 void ConfigureTimeForRunTimeStats(void)
@@ -51,4 +55,3 @@ void TIM3_IRQHandler(void)
         FreeRTOSRunTimeTicks++;
     }
 }
- 
--- user/APP/FreeRTOS_middleware/FreeRTOS_middleware.h
+++ user/APP/FreeRTOS_middleware/FreeRTOS_middleware.h
@@ -2,6 +2,8 @@
   ****************************(C) COPYRIGHT 2016 DJI****************************
   * @file       freeRTOS_middle.c/h
   * @brief      freeRTOS的中间层，将滴答计时中断和统计任务用时的接口函数放到这里.
+  *             middleware for freeRTOS
+  *             place the interface function for tick timer interrupt and task stats here
   * @history
   *  Version    Date            Author          Modification
   *  V1.0.0     Dec-26-2018     RM              1. 完成
